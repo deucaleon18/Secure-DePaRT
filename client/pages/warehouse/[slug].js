6ef64b17630bb794d4cab58806487a681a03ed55
Venu <venu.choudhary2000@gmail.com>
2022-07-31 03:52:15 +0530

    [ADDED] Camera component

--- client/pages/warehouse/[slug].js
+++ client/pages/warehouse/[slug].js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Hero from '../../Components/Hero'
+import SideModal from '../../Components/SideModal';
 
 const WareHouse = () => {
+  const [show,setShow]=useState(false);
+
   return (
     <>
       <Hero
         img={"warehouse.png"}
         des={"You have successfully logged in through a wareHouse account"}
         lable={"Check in product"}
-        action={()=>{}}
+        action={()=>setShow(true)}
       />
+      <SideModal title={"Check-In Product"}/>
     </>
   );
 }

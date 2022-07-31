30c570ee208f973e145093695ec2e3aa35e9c615
shashank725 <shashank.nov725@gmail.com>
2022-07-31 17:29:45 +0530

    [EDITED] Package API

--- backend/models/productTest.py
+++ backend/models/productTest.py
@@ -2,17 +2,17 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from productCheck import Package
 
-# obj = Package()
-# obj.create_and_train_model()
-# print(obj.predict())
+obj = Package()
+obj.create_and_train_model()
+print(obj.predict())
 
-import cv2
+# import cv2
 
-videoCaptureObject = cv2.VideoCapture(0)
-result = True
-while(result):
-    ret,frame = videoCaptureObject.read()
-    cv2.imwrite("NewPicture.jpg",frame)
-    result = False
-videoCaptureObject.release()
-cv2.destroyAllWindows()
+# videoCaptureObject = cv2.VideoCapture(0)
+# result = True
+# while(result):
+#     ret,frame = videoCaptureObject.read()
+#     cv2.imwrite("NewPicture.jpg",frame)
+#     result = False
+# videoCaptureObject.release()
+# cv2.destroyAllWindows()
